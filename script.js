e422f42eb5815e7834d6d2cf06759eec4ded8a7b
kristiyan <kristiyan.tefov@icloud.com>
2023-06-29 17:55:49 +0100

    fixing error not giving any message if you havent't select any characters

--- script.js
+++ script.js
@@ -31,8 +31,9 @@ function generatePassword() {
   // Ask the user if they want to include special characters
   const specialPrompt = confirm("Click OK to include special characters");
 
-  if(!lowerPrompt || !upperPrompt || !numbersPrompt || !special) {
+  if(!lowerPrompt || !upperPrompt || !numbersPrompt || !specialPrompt) {
     alert("You must choose at least one characteristic")
+    return 
   }
   // For loop to choose a random characters from the selecetions above 
   for (var i = 0; i < charLength; i++) {
